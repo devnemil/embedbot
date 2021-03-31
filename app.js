2bba194c9dffb920fec3c7b84aed4d292344bfd8
devnemil <73438564+devnemil@users.noreply.github.com>
2021-04-01 02:01:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -109,3 +109,9 @@ client.on('message', message => {
   }});
 
 client.login(process.env.token);
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
